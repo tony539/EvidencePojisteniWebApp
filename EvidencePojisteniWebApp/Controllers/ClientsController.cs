50e38d07655f895fd4140c1ef6f1321371cb93e5
tonydevIV <antoninjaros03@gmail.com>
2024-10-10 22:11:47 +0200

    Add authorize for admins

--- EvidencePojisteniWebApp/Controllers/ClientsController.cs
+++ EvidencePojisteniWebApp/Controllers/ClientsController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EvidencePojisteniWebApp.Data;
 using EvidencePojisteniWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EvidencePojisteniWebApp.Controllers
 {
+    [Authorize(Roles = UserRoles.Admin)]
     public class ClientsController : Controller
     {
         private readonly ApplicationDbContext _context;
